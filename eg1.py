1b76e87a3dda2f5333b66bb0913f4df806377fe2
dong.pei <dong.pei@phicomm.com>
2017-03-15 14:52:36 +0800

    first exercise

--- eg1.py
+++ eg1.py
@@ -5,5 +5,14 @@
 # @File    : eg1.py
 # @Software: PyCharm
 """
-here is it
+题目：有四个数字：1、2、3、4，能组成多少个互不相同且无重复数字的三位数？各是多少？
 """
+num=0
+for i in range(1,5):
+    for j in range(1,5):
+        for k in range(1,5):
+            if (i!=j and j!=k and i!=k):
+                num+=1
+                print(i,j,k)
+
+print(num)
---
+++ eg3.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author  : Peidong
+# @Site    : 
+# @File    : eg3.py
+# @Software: PyCharm
